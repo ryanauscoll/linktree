de73fe4fe8f9ba78aee5bdb8c09848a4d739803d
icekeys <ryancollingham5547@gmail.com>
2024-02-08 20:31:40 -0500

    site name

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,26 +1,11 @@
 import { DefaultSeoProps } from "next-seo";
 
 const config: DefaultSeoProps = {
-    title: "unamona",
+    title: "ryan.dev",
     description: "Navigating the Cosmos of Social Media: Explore with @UnitAndro 🚀",
-    themeColor: "#ff7bae",
-    openGraph: {
-        "title": "unamona",
-        type: "website",
-        description: "Navigating the Cosmos of Social Media: Explore with @UnitAndro 🚀",
-        siteName: "unamona"
-    },
-    twitter: {
-        handle: "unamona",
-        site: "unamona",
-        cardType: "summary_large_image"
-    },
-    additionalLinkTags: [
-        {
-            "rel": "icon",
-            "href": "favicon.png"
-        }
-    ]
+    themeColor: "#063970",
+    
+
 }
 
 export default config;
